fcde2319af7e83393cdc60eb01254f2da283e974
Kishan2182 <kishanmishra2182002@gmail.com>
2023-10-14 16:50:18 +0530

    changes made in signup page

--- Quizz_maker/lib/components/signup.dart
+++ Quizz_maker/lib/components/signup.dart
@@ -44,7 +44,7 @@ class _SignupState extends State<Signup> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         centerTitle: true,
-        // title: appBar(context),
+        title: appBar(context),
         backgroundColor: Colors.blueAccent,
         elevation: 0.0,
         systemOverlayStyle: SystemUiOverlayStyle.light,
